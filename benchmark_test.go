82aaf1e5e730e515baf496d565831db7ad4606b3
Tommy Chen <tommy351@gmail.com>
2020-07-05 17:52:20 +0800

    Add benchmarks

---
+++ benchmark_test.go
@@ -0,0 +1,49 @@
+package rdb
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"testing"
+)
+
+func BenchmarkParser(b *testing.B) {
+	benchmarkDumpFile := func(b *testing.B, name string) {
+		buf, err := ioutil.ReadFile(fmt.Sprintf("fixtures/%s.rdb", name))
+
+		if err != nil {
+			b.Error(err)
+		}
+
+		b.ResetTimer()
+		b.ReportAllocs()
+
+		for i := 0; i < b.N; i++ {
+			parser := NewParser(bytes.NewBuffer(buf))
+
+			for {
+				_, err := parser.Next()
+
+				if err == io.EOF {
+					break
+				}
+
+				if err != nil {
+					b.Error(err)
+				}
+			}
+		}
+	}
+
+	for _, name := range []string{
+		"empty_database",
+		"multiple_databases",
+		"keys_with_expiry",
+	} {
+		name := name
+		b.Run(name, func(b *testing.B) {
+			benchmarkDumpFile(b, name)
+		})
+	}
+}
--- main_test.go
+++ main_test.go
@@ -9,5 +9,5 @@ import (
 
 func Test(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "goldga")
+	RunSpecs(t, "rdb")
 }
