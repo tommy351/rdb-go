ad69ed4880879698d87bf63bbde14c743d417cfa
Tommy Chen <tommy351@gmail.com>
2020-07-05 13:01:02 +0800

    Update examples

--- example_test.go
+++ example_test.go
@@ -3,9 +3,18 @@ package rdb
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 func ExampleParser() {
+	file, err := os.Open("dump.rdb")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
 	parser := NewParser(file)
 
 	for {
@@ -22,6 +31,11 @@ func ExampleParser() {
 		switch data := data.(type) {
 		case *StringData:
 			fmt.Println(data.Key, data.Value)
+
+		case *ListData:
+			for i, value := range data.Value {
+				fmt.Println(data.Key, i, value)
+			}
 		}
 	}
 }
